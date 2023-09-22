5dc4ae37be9c824f2dac5871e520427bc8c2c7b3
Wilianto Indrawan <indrawan.wilianto@gmail.com>
2023-09-23 00:46:07 +0800

    Add room columns

--- ent/schema/room.go
+++ ent/schema/room.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
 
 // Room holds the schema definition for the Room entity.
 type Room struct {
@@ -9,7 +15,12 @@ type Room struct {
 
 // Fields of the Room.
 func (Room) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New).Unique().Immutable(),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now),
+		field.String("name"),
+	}
 }
 
 // Edges of the Room.
